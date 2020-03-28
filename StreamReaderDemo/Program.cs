dc10264812a3d2332dc3a188a058fb2efa99e7a5
yangkaige <1546865912@qq.com>
2020-03-28 21:59:42 +0800

    添加StreamReader&StreamWriter的使用示例

---
+++ StreamReaderDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace StreamReaderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("readme.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StreamWriterDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace StreamWriterDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter sw = new StreamWriter("readme.txt"))
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    sw.WriteLine(i);
+                }
+            }
+            Console.WriteLine("写入成功！！");
+            Console.ReadKey();
+        }
+    }
+}
